a12801f09dab1fe2c55c756622ed95e84cbf8b0d
Akvaro03 <68175898+Akvaro03@users.noreply.github.com>
2023-03-06 00:14:39 -0300

    .

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -7,12 +7,24 @@ import {
 import Home from "../pages/home/home";
 import Navbar from "../components/navbar/Navbar";
 
-export  const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Navbar />} >
-      <Route path="/store-react-blue" element={<Home />} />
-      <Route path="/store-react-blue/navbar" element={<Navbar />} />
-    </Route>
-  ),{basename: "/store-react-blue"}
-);
+// export  const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Home />} >
+//       <Route path="/store-react-blue" element={<Home />} />
+//       <Route path="/store-react-blue/navbar" element={<Navbar />} />
+//     </Route>
+//   ),{basename: "/store-react-blue"}
+// );
 
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "team",
+        element: <Navbar />,
+      },
+    ],
+  },
+],{basename: "/store-react-blue"});
